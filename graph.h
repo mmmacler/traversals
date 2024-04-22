df352f328a67b5219bf204cf2835cf6f4a02a23e
mmmacler <157934318+mmmacler@users.noreply.github.com>
2024-04-22 16:33:27 -0400

    small change to x_max and y_max

--- graph.h
+++ graph.h
@@ -6,8 +6,8 @@
 using namespace::std;
 //#define x_max 1840
 //#define y_max 1000
-#define x_max 400
-#define y_max 300
+#define x_max 300
+#define y_max 170
 
 #define multiplier 14
 #define vertex_radius 6.5
