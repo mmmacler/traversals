415f9f08218e1098a3a261cbb42c3442908a8003
mmmacler <157934318+mmmacler@users.noreply.github.com>
2024-04-22 23:47:49 -0400

    Update graph.h

--- graph.h
+++ graph.h
@@ -4,30 +4,22 @@
 #include <map>
 #include <random>
 using namespace::std;
-//#define x_max 1840
-//#define y_max 1000
-
-#define multiplier 14
-#define vertex_radius 6.5
-#define private public
 
 class Graph {
 private:
+
+    // This method generates the vertices in our graph,
+    // as well as the edges to go along with them.
+    void initialize_graph(int xmax, int ymax);
+
+public:
     // Maps the coordinate location of a vertex(first) to an adjacency list of other vertices(second)
         //The first value (pair) is the coordinate pair location of the vertex 
         //that this vertex is adjacent to.
         //The second value (float) is the weight of the edge.
     map<pair<int, int>, map<pair<int, int>, float>> vertices;
+    Graph(int xmax, int ymax);
 
-    // This method generates the 100,000 vertices in our graph,
-    // as well as the edges to go along with them.
-    void initialize_graph();
-    int numEdges;
-    int numVertices;
-
-public:
-    Graph();
-    // returns sum of shortest distance
     vector<pair<int, int>> BFS(pair<int, int> from, pair<int, int> to);
     vector<pair<int, int>> DFS(pair<int, int> from, pair<int, int> to);
 };
